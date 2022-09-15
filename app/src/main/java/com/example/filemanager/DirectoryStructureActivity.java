7a31d12386ebe4e9ce025c38a08cabfd003ab79c
Besfort21 <besfort-avdyli1@hotmail.com>
2022-09-15 20:14:10 +0200

    Request permissions, first page and get directory structure

---
+++ app/src/main/java/com/example/filemanager/DirectoryStructureActivity.java
@@ -0,0 +1,34 @@
+package com.example.filemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class DirectoryStructureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_directory_structure);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        TextView noFilesText = findViewById(R.id.noFilesTextView);
+
+        String path = getIntent().getStringExtra("path");
+
+        File root = new File(path);
+        File[] directoryStructure = root.listFiles();
+
+        if(directoryStructure == null || directoryStructure.length == 0){
+            noFilesText.setVisibility(View.VISIBLE);
+            return;
+        }
+
+        noFilesText.setVisibility(View.INVISIBLE);
+    }
+}
--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.filemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton storageBtn = findViewById(R.id.storageBtn);
+
+        storageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkPermission()){
+                    //permission
+                    Intent intent = new Intent(MainActivity.this,DirectoryStructureActivity.class);
+                    String path = Environment.getExternalStorageDirectory().getPath();
+                    intent.putExtra("path",path);
+                    startActivity(intent);
+                }
+                else{
+                    //no permission
+                    requestPermission();
+                }
+            }
+        });
+    }
+
+    private boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            Toast.makeText(MainActivity.this,"Storage permission is required,allow from settings",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},111);
+
+        }
+
     }
 }
